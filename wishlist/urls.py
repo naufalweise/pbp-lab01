b9a333b3c9f67144a5637ccae3df5c66cac635ff
Naufal Weise <naufalwwidyatama@gmail.com>
2022-09-22 15:35:54 +0700

    Add authentication

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from wishlist.views import show_wishlist, get_xml, get_json, get_json_by_id, get_xml_by_id
-
+from wishlist.views import show_wishlist, get_xml, get_json, get_json_by_id, get_xml_by_id, register
+from . import views
 app_name = 'wishlist'
 
 urlpatterns = [
@@ -8,5 +8,8 @@ urlpatterns = [
     path('xml/', get_xml, name='get_xml'),
     path('json/', get_json, name='get_json'),
     path('json/<int:id>', get_json_by_id, name='get_json_by_id'),
-    path('xml/<int:id>', get_xml_by_id, name='get_xml_by_id')
+    path('xml/<int:id>', get_xml_by_id, name='get_xml_by_id'),
+    path('register/', register, name="register"),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
